d8f900bab2d6a6177da42d51549f36502ed7cd49
DreamReaperLEE <qq26407166@gmail.com>
2016-03-12 22:19:37 +0800

    后端完成

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,6 +6,7 @@ class login extends CI_Controller {
         parent::__construct();
         $this->load->model('login_model');
         $this->load->helper('url_helper');
+        $this->load->library('session');
     }
     public function index(){
         $this->load->view('templates/header.php');
@@ -15,16 +16,25 @@ class login extends CI_Controller {
     public function log()
     {
         $id = $this->input->post('id');
-        $password=$this->input->post('password');
+        $password = $this->input->post('password');
         $login= $this->login_model->getlog($id);
-        if($password==$login['t_password']){
-            if($login['t_power']==1)    //1为普通管理员
+        if($login==NULL){
+            echo '没有这个用户';
+        }
+        else if($password==$login['t_password']){
+            if($login['t_power']=='1'){    //1为普通管理员
                 echo '管理员';
-            else                        //0为普通老师
+                $this->session->set_tempdata('usertype', '1', 300);
+                echo $_SESSION['usertype'];
+            }
+            else{                        //0为普通老师
                 echo '普通老师';
+               $this->session->set_tempdata('usertype', '0', 300);
+                echo $_SESSION['usertype'];
+            }
         }
         else{
-            $this->load->view('errors/html/error_general.php');     //密码错误处理方式
+            echo '帐号或密码错误';     //密码错误处理方式
         }
     }
  
